f426b2d8d89eb3eef8f29003e504b0cc78cf28c8
dengdairiluo <1020447548@qq.com>
2020-12-27 16:35:03 +0800

    20201227

---
+++ src/main/java/com/yuren/leetcodecnlearn/J64.java
@@ -0,0 +1,14 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2020-12-27 16:34
+ */
+public class J64 {
+    public int sumNums(int n) {
+        return n * (n + 1) / 2;
+    }
+}

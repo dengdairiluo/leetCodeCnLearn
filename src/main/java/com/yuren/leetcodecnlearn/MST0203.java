c05c94839987b73459b50969d70604a377382e64
Alex <1020447548@qq.com>
2021-01-08 10:09:02 +0800

    20210108

---
+++ src/main/java/com/yuren/leetcodecnlearn/MST0203.java
@@ -0,0 +1,18 @@
+package com.yuren.leetcodecnlearn;
+
+public class MST0203 {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}

fd801151ab0ed3c3e7b9cfc81838866e1bdaf9e6
dengdairiluo <1020447548@qq.com>
2021-01-17 20:23:21 +0800

    20210116,20210117ˆˆ

---
+++ src/main/java/com/yuren/leetcodecnlearn/MST101.java
@@ -0,0 +1,17 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-01-17 20:22
+ */
+public class MST101 {
+    public int[] swapNumbers(int[] numbers) {
+        numbers[0] = numbers[0] + numbers[1];
+        numbers[1] = numbers[0] - numbers[1];
+        numbers[0] = numbers[0] - numbers[1];
+        return numbers;
+    }
+}

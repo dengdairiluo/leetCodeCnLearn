165047fcf2d0a9d66466d281c08045ea3891f22a
Alex <1020447548@qq.com>
2021-02-08 09:26:01 +0800

    20210208

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1290.java
@@ -0,0 +1,22 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q1290 {
+
+    public static int getDecimalValue(ListNode head) {
+        int ans = 0;
+
+        while(head != null) {
+            ans = (ans << 1) + head.val;
+            head = head.next;
+        }
+        return ans;
+    }
+
+    
+      public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+      }
+   
+}

cce3d7f41da45b2cdfe9d2c5c2c14d1915ec6972
Alex <1020447548@qq.com>
2021-02-23 10:10:34 +0800

    20210223

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1221.java
@@ -0,0 +1,46 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q1221 {
+
+    private static int balancedStringSplit1(String s) {
+        int ans = 0;
+
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'L') {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            if (cnt == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private static int balancedStringSplit2(String s) {
+        int ans = 0;
+
+        int cnt = 0;
+
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == 'L') {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            if (cnt == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "RLRRLLRLRL";
+        System.out.println(balancedStringSplit1(s));
+    }
+}

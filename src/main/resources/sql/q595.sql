6a55185c6f812f4970ba872828b0f30d586ba8fd
Alex <1020447548@qq.com>
2021-02-24 14:48:19 +0800

    20210224

---
+++ src/main/resources/sql/q595.sql
@@ -0,0 +1,22 @@
+select 
+name, population, area
+from World 
+where 
+population > 25000000
+or area > 3000000;
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area > 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population > 25000000;

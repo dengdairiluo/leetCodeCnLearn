4b97139d1fdb287ad09d397f04cf14b96e9c8955
Alex <1020447548@qq.com>
2021-03-22 20:57:44 +0800

    20210322

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q191.java
@@ -0,0 +1,22 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q191 {
+
+    public int hammingWeight1(int n) {
+        int count = 0;
+        while(n!=0){
+            count+= n&1;
+            n >>>= 1;
+        }
+        return count;
+    }
+
+    public int hammingWeight2(int n) {
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
+    }
+}

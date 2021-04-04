b4926481c44169b7c3ed61c3372e29d96b289072
dengdairiluo <1020447548@qq.com>
2021-04-04 18:31:59 +0800

    20210404

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q5705.java
@@ -0,0 +1,18 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-04-04 18:31
+ */
+public class Q5705 {
+
+    public boolean squareIsWhite(String coordinates) {
+        int x = coordinates.charAt(0) - 'a' + 1;
+        int y = coordinates.charAt(1) - '0';
+        return (x & 1) == 1 ? (y & 1) != 1 : (y & 1) != 0;
+    }
+
+}

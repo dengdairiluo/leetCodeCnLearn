71e7ac82fd70a5bb8b144fce7ca345bedf241345
Alex <1020447548@qq.com>
2021-04-20 21:16:19 +0800

    20210420.1

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1822.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q1822 {
+
+    private static int arraySign1(int[] nums) {
+        int cnt = 0;
+        for (int num : nums) {
+            if (num == 0)
+                return 0;
+            if (num < 0) {
+                cnt++;
+            }
+        }
+        return cnt % 2 == 1 ? -1 : 1;
+    }
+
+    private static int arraySign2(int[] nums) {
+        int ans = 1;
+        for (int num : nums) {
+            if (num == 0)
+                return 0;
+            if (num < 0) {
+                ans *= -1;
+            }
+        }
+        return ans;
+    }
+
+}

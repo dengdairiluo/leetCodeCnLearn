e50540f0b51fecd536b595b4517274b2fbd9c69f
dengdairiluo <1020447548@qq.com>
2021-05-05 21:35:29 +0800

    20210505

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q977.java
@@ -0,0 +1,23 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-05-05 21:28
+ */
+public class Q977 {
+    public int[] sortedSquares(int[] nums) {
+
+        int len = nums.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}

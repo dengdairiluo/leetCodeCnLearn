714fac58876d3eb12ad6b2966b933e342599b809
Alex <1020447548@qq.com>
2021-05-10 21:01:31 +0800

    20210510.2

---
+++ src/main/java/com/yuren/leetcodecnlearn/J25.java
@@ -0,0 +1,41 @@
+package com.yuren.leetcodecnlearn;
+
+public class J25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //递归实现
+        return recur(l1, l2);
+    }
+
+    public ListNode recur(ListNode l1, ListNode l2) {
+        //是否为null判断
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        //新建头结点
+        ListNode head = null;
+
+        //如果l1.val <= l2.val，那么头结点的值为l1.head的值，然后开始递归
+        if (l1.val <= l2.val) {
+            head = new ListNode(l1.val);
+            head.next = recur(l1.next, l2);
+        }
+        //否则，头结点的值为l2.head的值，然后开始递归
+        else {
+            head = new ListNode(l2.val);
+            head.next = recur(l1, l2.next);
+        }
+
+        //返回该链表
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}

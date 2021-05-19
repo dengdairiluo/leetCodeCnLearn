47dfed19ea58cad445bac8b9d67301c2348cac3a
Alex <1020447548@qq.com>
2021-05-19 20:28:32 +0800

    20210519

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q933.java
@@ -0,0 +1,18 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q933 {
+    Queue<Integer> q;
+    public Q933() {
+        q = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        q.add(t);
+        while (q.peek() < t - 3000)
+            q.poll();
+        return q.size();
+    }
+}

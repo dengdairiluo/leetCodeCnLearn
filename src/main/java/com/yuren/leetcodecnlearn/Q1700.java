80e6fb623a590e9347beb478884de1408efc9ac6
dengdairiluo <1020447548@qq.com>
2021-05-20 21:12:22 +0800

    2021052020210520

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1700.java
@@ -0,0 +1,37 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-05-20 21:11
+ */
+public class Q1700 {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int n = students.length;
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int student : students) {
+            queue.offer(student);
+        }
+        int j = 0;//三明治栈顶的元素位置
+        int count = 0;//记录吃过三明治的学生数量
+        int currSize = -1;
+        while (queue.size() != currSize) {
+            currSize = queue.size();
+            for (int i = 0; i < currSize; i++) {
+                int value = queue.remove();
+                if (value == sandwiches[j]) {
+                    j++;
+                    count++;
+                } else {
+                    queue.offer(value);
+                }
+            }
+        }
+        return n - count;
+    }
+}

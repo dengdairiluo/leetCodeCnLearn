581a947b77a43d4c92fc5966a6618ef4d20897e9
dengdairiluo <1020447548@qq.com>
2021-05-22 22:00:49 +0800

    20210522

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q359.java
@@ -0,0 +1,40 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.HashMap;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-05-22 21:57
+ */
+public class Q359 {
+
+    private final HashMap<String, Integer> msgDict;
+
+    /** Initialize your data structure here. */
+    public Q359() {
+        msgDict = new HashMap<>();
+    }
+
+    /**
+     * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+
+        if (!this.msgDict.containsKey(message)) {
+            this.msgDict.put(message, timestamp);
+            return true;
+        }
+
+        Integer oldTimestamp = this.msgDict.get(message);
+        if (timestamp - oldTimestamp >= 10) {
+            this.msgDict.put(message, timestamp);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}

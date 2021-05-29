adf05d824a898323b601e762ab95b9a92e06334f
dengdairiluo <1020447548@qq.com>
2021-05-29 20:57:30 +0800

    20210529

--- src/main/java/com/yuren/leetcodecnlearn/Q521.java
+++ src/main/java/com/yuren/leetcodecnlearn/Q521.java
@@ -1,7 +1,18 @@
-package com.yuren.leetcodecnlearn;/**
+package com.yuren.leetcodecnlearn;
+
+/**
  * Created with Intellij IDEA.
  * Description:
+ *
  * @author lujiang
  * @date 2021-05-29 20:21
- */public class Q521 {
+ */
+public class Q521 {
+    public int findLusLength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+
+    }
 }

c58ad62b350da77cee4ad77bbfe7dbd1847fc7de
Alex <1020447548@qq.com>
2021-06-02 21:05:07 +0800

    20210602

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1742.java
@@ -0,0 +1,48 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q1742 {
+    public int countBalls(int lowLimit, int highLimit) {
+        // 盒子所有可能编号为下标组成的数组
+        int[] arr = new int[46];
+        // 第一个小球对应的盒子的编号
+        int index = sum(lowLimit);
+        arr[index]++;
+        // 将小球放入对应编号的盒子中
+        for (int i = lowLimit + 1; i <= highLimit; i++) {
+            // 利用前一个小球对应盒子的编号，
+            // 求出后一个小球对应盒子的编号
+            int endZeroCount = endZeroCount(i);
+            index += 1 - 9 * endZeroCount;
+            arr[index]++;
+        }
+        // 找出最多小球的盒子中的小球数量
+        int maxNum = 0;
+        for (int j : arr) {
+            if (j > maxNum) {
+                maxNum = j;
+            }
+        }
+        return maxNum;
+    }
+
+    //数字的各个位之和
+    public int sum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    //数字末尾连续0的个数
+    public int endZeroCount(int num) {
+        int endZeroCount = 0;
+        while (num % 10 == 0) {
+            num = num / 10;
+            endZeroCount++;
+        }
+        return endZeroCount;
+
+    }
+}

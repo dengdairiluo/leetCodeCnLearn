6eb8d7e830136b28f7bf41ea4f5851dc6e48fcbf
Alex <1020447548@qq.com>
2021-06-10 20:39:51 +0800

    20210610

---
+++ src/main/java/com/yuren/leetcodecnlearn/J6802.java
@@ -0,0 +1,69 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class J6802 {
+    private TreeNode ans;
+
+    public J6802() {
+        this.ans = null;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean lSon = dfs(root.left, p, q);
+        boolean rSon = dfs(root.right, p, q);
+        if ((lSon && rSon) || ((root.val == p.val || root.val == q.val) && (lSon || rSon))) {
+            ans = root;
+        }
+        return lSon || rSon || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    Map<Integer, TreeNode> parent = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+
+    public void dfs(TreeNode root) {
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
+}

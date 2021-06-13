e7feb2590bcf53bf669d6ebb984dd363f5abefbc
dengdairiluo <1020447548@qq.com>
2021-06-13 23:44:22 +0800

    20210613

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1710.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-06-13 23:43
+ */
+public class Q1710 {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int count = 0;
+        int[] ans = new int[1001];
+        for (int[] is : boxTypes) {
+            ans[is[1]] += is[0];
+        }
+        for (int i = ans.length - 1; i > 0; i--) {
+            if (ans[i] != 0) {
+                if (truckSize > ans[i]) {
+                    count += ans[i] * i;
+                    truckSize -= ans[i];
+                } else {
+                    return count + truckSize * i;
+                }
+            }
+        }
+        return count;
+    }
+}

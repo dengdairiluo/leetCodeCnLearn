ba457fcb1562693ce49780eaa8ea8938e67fc27a
Alex <1020447548@qq.com>
2021-06-15 21:41:03 +0800

    20210615

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q944.java
@@ -0,0 +1,16 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q944 {
+    public int minDeletionSize(String[] strs) {
+        int ans = 0;
+        for (int c = 0; c < strs[0].length(); ++c)
+            for (int r = 0; r < strs.length - 1; ++r)
+                if (strs[r].charAt(c) > strs[r + 1].charAt(c)) {
+                    ans++;
+                    break;
+                }
+
+        return ans;
+
+    }
+}

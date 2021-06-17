7c8d2924eb80f28a31b1822a87e49e0c4b9f6ccf
dengdairiluo <1020447548@qq.com>
2021-06-17 23:33:59 +0800

    20210617

---
+++ src/main/java/com/yuren/leetcodecnlearn/MST0207.java
@@ -0,0 +1,31 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-06-17 23:32
+ */
+public class MST0207 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode h1 = headA, h2 = headB;
+
+        while (h1 != h2) {
+
+            h1 = h1 == null ? headB : h1.next;
+            h2 = h2 == null ? headA : h2.next;
+        }
+
+        return h1;
+    }
+    
+      private static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+}

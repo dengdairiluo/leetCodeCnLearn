cc4297bb14ccdc70dec021fe3f1ff59786e3073c
Alex <1020447548@qq.com>
2021-06-19 15:47:08 +0800

    20210619.1

---
+++ src/main/resources/sql/q1767.sql
@@ -0,0 +1,9 @@
+with recursive t(task_id, subtask_id) as
+    (
+    select task_id, subtasks_count from Tasks
+    union all
+    select task_id, subtask_id - 1 from t where subtask_id >= 2
+    )
+
+select t.* from t left join Executed e using(task_id, subtask_id)
+where e.subtask_id is null

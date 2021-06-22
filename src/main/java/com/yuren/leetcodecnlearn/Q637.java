44ef3d468c0f09ce5b3953fe6bdc136f2377fdcb
Alex <1020447548@qq.com>
2021-06-22 19:49:26 +0800

    20210622

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q637.java
@@ -0,0 +1,54 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q637 {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Integer> counts = new ArrayList<>();
+        List<Double> sums = new ArrayList<>();
+        dfs(root, 0, counts, sums);
+        List<Double> averages = new ArrayList<>();
+        int size = sums.size();
+        for (int i = 0; i < size; i++) {
+            averages.add(sums.get(i) / counts.get(i));
+        }
+        return averages;
+    }
+
+    public void dfs(TreeNode root, int level, List<Integer> counts, List<Double> sums) {
+        if (root == null) {
+            return;
+        }
+        if (level < sums.size()) {
+            sums.set(level, sums.get(level) + root.val);
+            counts.set(level, counts.get(level) + 1);
+        } else {
+            sums.add(1.0 * root.val);
+            counts.add(1);
+        }
+        dfs(root.left, level + 1, counts, sums);
+        dfs(root.right, level + 1, counts, sums);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

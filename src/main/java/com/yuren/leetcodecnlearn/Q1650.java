979aa029e225237dad51e0067f3a10723481f5b9
Alex <1020447548@qq.com>
2021-06-23 19:46:10 +0800

    20210623

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1650.java
@@ -0,0 +1,23 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q1650 {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Node a = p, b = q;
+        while (a != b) {
+            a = (a.parent == null) ? q : a.parent;
+            b = (b.parent == null) ? p : b.parent;
+        }
+        return a;
+    }
+
+    private static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    ;
+
+
+}

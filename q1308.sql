31186e325e6bc098e777dac4ad93176701fe27de
Alex <1020447548@qq.com>
2021-07-07 17:05:02 +0800

    Delete q1308.sql

--- q1308.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT s1.gender, s1.day, SUM(s2.score_points) AS total
-FROM Scores AS s1 JOIN Scores AS s2
-                       ON s1.gender = s2.gender AND s1.day >= s2.day
-GROUP BY s1.gender, s1.day
-ORDER BY s1.gender, s1.day;

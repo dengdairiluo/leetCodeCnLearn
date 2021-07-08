258951b1e9dbf4abbe40ad6e6fdba254dcef4cb0
Alex <1020447548@qq.com>
2021-07-08 19:56:25 +0800

    20210708

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q281.java
@@ -0,0 +1,45 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q281 {
+    private ArrayList<Integer>[] listArr;
+    private int i;
+    private int j;
+    private int size;
+
+    public Q281(List<Integer> v1, List<Integer> v2) {
+        listArr = new ArrayList[2];
+        listArr[0] = new ArrayList<>(v1);
+        listArr[1] = new ArrayList<>(v2);
+        size = Math.max(v1.size(), v2.size());
+        i = v1.size() > 0 ? 0 : 1;
+        j = 0;
+    }
+
+    public int next() {
+        int ans = listArr[i].get(j);
+        moveToNext();
+        return ans;
+    }
+
+    public boolean hasNext() {
+        return i < listArr.length && j < size;
+    }
+
+    private void moveToNext() {
+        movePoint();
+        if (j >= listArr[i].size()) {
+            movePoint();
+        }
+    }
+
+    private void movePoint() {
+        i += 1;
+        if (i == listArr.length) {
+            i = 0;
+            j += 1;
+        }
+    }
+}

4f2cd3aca97e6d2fbfb9bf4286b9cf318f91f1a2
dengdairiluo <1020447548@qq.com>
2021-07-16 22:29:05 +0800

    20210716

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q230.java
@@ -0,0 +1,45 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.LinkedList;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-07-16 22:24
+ */
+public class Q230 {
+    public int kthSmallest(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

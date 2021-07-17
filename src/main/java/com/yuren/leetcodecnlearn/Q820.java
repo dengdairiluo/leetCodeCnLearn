4e02fde599568dd673e0e5743737c05d90d58a9d
dengdairiluo <1020447548@qq.com>
2021-07-17 21:17:31 +0800

    20210717

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q820.java
@@ -0,0 +1,22 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-07-17 21:16
+ */
+public class Q820 {
+    public boolean xorGame(int[] nums) {
+        if (nums.length % 2 == 0) {
+            return true;
+        }
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor == 0;
+
+    }
+}

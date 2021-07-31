317035a723d13f2997263f0a35eb0b581a59efcd
dengdairiluo <1020447548@qq.com>
2021-07-31 19:12:13 +0800

    20210731

---
+++ src/main/resources/sql/q1204.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT a.person_name
+FROM Queue a, Queue b
+WHERE a.turn >= b.turn
+GROUP BY a.person_id HAVING SUM(b.weight) <= 1000
+ORDER BY a.turn DESC
+LIMIT 1

f20479304efbd6e4c5cecc527d6f2bd352e3c979
dengdairiluo <1020447548@qq.com>
2021-08-01 22:08:19 +0800

    20210801

---
+++ src/main/java/com/yuren/leetcodecnlearn/Mst0107.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-08-01 22:08
+ */
+public class Mst0107 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+}

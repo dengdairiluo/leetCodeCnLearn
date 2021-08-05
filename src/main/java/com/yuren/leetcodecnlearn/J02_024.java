d96727758956ac8729f2abb848ed76eeb2e02b09
dengdairiluo <1020447548@qq.com>
2021-08-05 23:31:25 +0800

    20210805

---
+++ src/main/java/com/yuren/leetcodecnlearn/J02_024.java
@@ -0,0 +1,42 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-08-05 23:27
+ */
+public class J02_024 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        //记录上一个节点
+        ListNode last = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = last;
+            last = head;
+            head = temp;
+        }
+        return last;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

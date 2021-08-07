6952f153eb79598b23462002f20bedc9623372eb
dengdairiluo <1020447548@qq.com>
2021-08-07 20:44:29 +0800

    20210807

---
+++ src/main/java/com/yuren/leetcodecnlearn/J02_054.java
@@ -0,0 +1,46 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-08-07 20:36
+ */
+public class J02_054 {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        inorder(root);
+        return root;
+    }
+
+    //改进版的中序遍历
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root.right);
+        sum += root.val;
+        root.val = sum;
+        inorder(root.left);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

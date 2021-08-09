f4d6b6690c4e9c98c32f8318d53cde96c2d062ec
Alex <1020447548@qq.com>
2021-08-09 18:33:35 +0800

    20210809

---
+++ src/main/java/com/yuren/leetcodecnlearn/J02042.java
@@ -0,0 +1,19 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class J02042 {
+    Queue<Integer> list;
+    public J02042() {//建立队列
+        list = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        list.add(t);//添加到队列
+        while(t-3000 > list.peek()){//超过3000差值即队头出列
+            list.poll();
+        }
+        return list.size();//返回大小
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/J02079.java
@@ -0,0 +1,27 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class J02079 {
+    private final List<List<Integer>> res = new ArrayList<>();
+    private final List<Integer> list = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        helper(0, nums);
+        return res;
+    }
+
+    //使用递归解决问题
+    public void helper(int len, int[] nums) {
+        if (len == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(nums[len]);
+        helper(len + 1, nums);
+        list.remove(list.size() - 1);
+        helper(len + 1, nums);
+    }
+
+}

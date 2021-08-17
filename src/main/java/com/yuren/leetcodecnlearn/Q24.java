b9e211d8e16e333a287d4eb89c9722ad63bf0a34
Alex <1020447548@qq.com>
2021-08-17 19:34:42 +0800

    20210817

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q24.java
@@ -0,0 +1,30 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q24 {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

fc4802642f992f1c27def37654e07067b9f37a9d
dengdairiluo <1020447548@qq.com>
2021-09-10 22:32:29 +0800

    20210910

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q92.java
@@ -0,0 +1,54 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-09-10 22:31
+ */
+public class Q92 {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        // 定义一个dummyHead, 方便处理
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        // 初始化指针
+        ListNode g = dummyHead;
+        ListNode p = dummyHead.next;
+
+        // 将指针移到相应的位置
+        for (int step = 0; step < m - 1; step++) {
+            g = g.next;
+            p = p.next;
+        }
+
+        // 头插法插入节点
+        for (int i = 0; i < n - m; i++) {
+            ListNode removed = p.next;
+            p.next = p.next.next;
+
+            removed.next = g.next;
+            g.next = removed;
+        }
+
+        return dummyHead.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

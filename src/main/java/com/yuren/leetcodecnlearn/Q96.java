e63ebe122c2353acce9f3afaa8932ea15955bc89
dengdairiluo <1020447548@qq.com>
2021-09-12 23:44:17 +0800

    20210912

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q96.java
@@ -0,0 +1,20 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-09-12 23:42
+ */
+public class Q96 {
+    public int numTrees(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+
+    }
+}

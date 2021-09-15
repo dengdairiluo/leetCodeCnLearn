a2b61579dee9525c599f612ea3a264afa101a8d4
Alex <1020447548@qq.com>
2021-09-15 21:02:21 +0800

    20210915

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q113.java
@@ -0,0 +1,48 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q113 {
+    private final List<List<Integer>> ret = new LinkedList<>();
+    private final Deque<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ret;
+    }
+
+    public void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        targetSum -= root.val;
+        if (root.left == null && root.right == null && targetSum == 0) {
+            ret.add(new LinkedList<Integer>(path));
+        }
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+        path.pollLast();
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

332e919704e7daeb06c91539b17bba284189b3c9
dengdairiluo <1020447548@qq.com>
2021-09-29 23:22:44 +0800

    20210929

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q154.java
@@ -0,0 +1,26 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-09-29 23:22
+ */
+public class Q154 {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+}

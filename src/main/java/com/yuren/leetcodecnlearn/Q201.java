4ac6b58bc9deaf635d63967b4800460d58fecd73
dengdairiluo <1020447548@qq.com>
2021-10-12 22:01:43 +0800

    20211012

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q201.java
@@ -0,0 +1,23 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-10-12 22:01
+ */
+public class Q201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == n) {
+            return m;
+        }
+        int a = ~(n ^ m);
+        a &= a >> 1;
+        a &= a >> 2;
+        a &= a >> 4;
+        a &= a >> 8;
+        a &= a >> 16;
+        return m & a;
+    }
+}

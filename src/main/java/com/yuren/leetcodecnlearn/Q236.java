8314d6dae5a971aeea3429c592d681e0f5122584
dengdairiluo <1020447548@qq.com>
2021-10-23 23:45:04 +0800

    20211021,20211022,20211023

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q236.java
@@ -0,0 +1,30 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-10-23 23:30
+ */
+public class Q236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 1.
+        if(left == null && right == null) return null;
+        // 3.
+        if(left == null) return right;
+        // 4.
+        if(right == null) return left;
+        return root; // 2. if(left != null and right != null)
+    }
+    
+      private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}

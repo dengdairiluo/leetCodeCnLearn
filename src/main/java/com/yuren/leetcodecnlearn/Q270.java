41aa56df0d6a6bfb63a3932cb288f7a459b32f6f
dengdairiluo <1020447548@qq.com>
2021-11-05 22:52:34 +0800

    20211105

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q270.java
@@ -0,0 +1,40 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-11-05 21:48
+ */
+public class Q270 {
+    public int closestValue(TreeNode root, double target) {
+        int val, closest = root.val;
+        while (root != null) {
+            val = root.val;
+            closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest;
+            root = target < root.val ? root.left : root.right;
+        }
+        return closest;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

c92066ee4720d00e19d2265b970d7adc12f998a0
dengdairiluo <1020447548@qq.com>
2021-11-07 23:36:42 +0800

    20211107

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q275.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-11-07 23:36
+ */
+public class Q275 {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
+
+    }
+}

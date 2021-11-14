5dbdeb0ed030637525deda39fc1650e3b86edaa4
dengdairiluo <1020447548@qq.com>
2021-11-14 22:07:22 +0800

    20211112,20211114

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q297.java
@@ -0,0 +1,46 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-11-14 22:04
+ */
+public class Q297 {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root == null){
+            return "null";
+        }
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return dfs(queue);
+    }
+
+    private TreeNode dfs(Queue<String> queue) {
+        String val = queue.poll();
+        if("null".equals(val)){
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+        root.left = dfs(queue);
+        root.right = dfs(queue);
+        return root;
+    }
+    
+    private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}

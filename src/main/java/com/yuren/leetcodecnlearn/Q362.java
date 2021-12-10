6e3686d2fb02068906198b12f4b454b611203608
dengdairiluo <1020447548@qq.com>
2021-12-11 00:00:21 +0800

    20211210.1

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q362.java
@@ -0,0 +1,51 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2021-12-10 23:58
+ */
+public class Q362 {
+    /**
+     * Initialize your data structure here.
+     */
+    Queue<Integer> q;
+    int count;
+
+    public Q362() {
+        q = new LinkedList<>();
+        count = 0;
+    }
+
+    /**
+     * Record a hit.
+     *
+     * @param timestamp - The current timestamp (in seconds granularity).
+     */
+    public void hit(int timestamp) {
+        q.add(timestamp);
+        count++;
+    }
+
+    /**
+     * Return the number of hits in the past 5 minutes.
+     *
+     * @param timestamp - The current timestamp (in seconds granularity).
+     */
+    public int getHits(int timestamp) {
+        if (q.size() == 0) return 0;
+        int peek = q.peek();
+        while (peek < timestamp - 299) {
+            q.poll();
+            count--;
+            if (q.isEmpty()) return 0;
+            peek = q.peek();
+        }
+        return count;
+    }
+}

da67f55cb46c27c8ed0374a56c6176e5578edf80
Alex <1020447548@qq.com>
2021-12-13 19:55:32 +0800

    20211213

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q372.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q372 {
+    private static final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        return dfs(a % MOD, b, b.length - 1);
+    }
+
+    private int dfs(int a, int[] b, int idx) {
+        if (idx == -1 || a == 1) return 1;
+        return qPow(dfs(a, b, idx - 1), 10) * qPow(a, b[idx]) % MOD;
+    }
+
+    private int qPow(int a, int b) {
+        int ans = 1;
+        a %= MOD;
+        while (b > 0) {
+            if ((b & 1) != 0) ans = ans * a % MOD;
+            a = a * a % MOD;
+            b >>= 1;
+        }
+        return ans;
+    }
+}

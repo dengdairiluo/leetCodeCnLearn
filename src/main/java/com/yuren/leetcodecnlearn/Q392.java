c4103fe1adfba772ae31c5b0267336dbd677a2b9
Alex <1020447548@qq.com>
2021-12-21 20:37:29 +0800

    20211221

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q392.java
@@ -0,0 +1,20 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q392 {
+    public boolean isSubsequence(String s, String t) {
+        int sLength = s.length();
+        int tLength = t.length();
+        int slow = 0;
+        int fast = 0;
+        for (; slow < sLength && fast < tLength; fast++) {
+            if (s.charAt(slow) == t.charAt(fast)) {
+                slow++;
+                if (slow == sLength) {
+                    //说明已经在t中找到了子序列 等于s
+                    break;
+                }
+            }
+        }
+        return slow == sLength;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q393.java
@@ -0,0 +1,17 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q393 {
+    public boolean validUtf8(int[] data) {
+        int index = 0;
+        while (index < data.length) {
+            if (data[index] >> 7 == 0) index++;
+            else if (index <= data.length - 2 && data[index] >> 5 == 6 && data[index + 1] >> 6 == 2) index += 2;
+            else if (index <= data.length - 3 && data[index] >> 4 == 14 && data[index + 1] >> 6 == 2 && data[index + 2] >> 6 == 2)
+                index += 3;
+            else if (index <= data.length - 4 && data[index] >> 3 == 30 && data[index + 1] >> 6 == 2 && data[index + 2] >> 6 == 2 && data[index + 3] >> 6 == 2)
+                index += 4;
+            else break;
+        }
+        return index == data.length;
+    }
+}

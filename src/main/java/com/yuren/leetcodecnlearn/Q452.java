592951753aa9091bb901683d31541dde899a3d8e
dengdairiluo <1020447548@qq.com>
2022-01-15 23:37:24 +0800

    20220115

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q452.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-01-15 23:32
+ */
+public class Q452 {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, Comparator.comparingInt(point -> point[1]));
+        int pos = points[0][1];
+        int ans = 1;
+        for (int[] balloon : points) {
+            if (balloon[0] > pos) {
+                pos = balloon[1];
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}

62ac7092acebc30b5cfb17e4c2fe3f2fdb432f15
dengdairiluo <1020447548@qq.com>
2022-01-16 22:59:32 +0800

    20220116

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q455.java
@@ -0,0 +1,28 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-01-16 22:58
+ */
+public class Q455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numOfChildren = g.length, numOfCookies = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

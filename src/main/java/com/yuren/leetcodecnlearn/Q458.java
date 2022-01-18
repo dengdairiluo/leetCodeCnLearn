3c188d87f393179239bde9e8e85b6bf9ffad8616
Alex <1020447548@qq.com>
2022-01-18 20:59:30 +0800

    20220118

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q458.java
@@ -0,0 +1,9 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q458 {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int states = minutesToTest / minutesToDie + 1;
+        int pigs = (int) Math.ceil(Math.log(buckets) / Math.log(states));
+        return pigs;
+    }
+}

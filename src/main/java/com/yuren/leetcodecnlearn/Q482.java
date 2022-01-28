d42b0382f280ced66d197e831657de860f1f371d
dengdairiluo <1020447548@qq.com>
2022-01-28 23:36:36 +0800

    20220128

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q482.java
@@ -0,0 +1,30 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-01-28 23:35
+ */
+public class Q482 {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder ans = new StringBuilder();
+        int cnt = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                cnt++;
+                ans.append(Character.toUpperCase(s.charAt(i)));
+                if (cnt % k == 0) {
+                    ans.append("-");
+                }
+            }
+        }
+        if (ans.length() > 0 && ans.charAt(ans.length() - 1) == '-') {
+            ans.deleteCharAt(ans.length() - 1);
+        }
+
+        return ans.reverse().toString();
+    }
+}

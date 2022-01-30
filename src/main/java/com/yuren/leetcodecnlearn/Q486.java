1e32221c361944cab5ed42d74eba1bf308fcb576
dengdairiluo <1020447548@qq.com>
2022-01-30 23:04:20 +0800

    20220130

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q486.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-01-30 23:04
+ */
+public class Q486 {
+    // 状态转移方程：dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
+
+    public boolean PredictTheWinner(int[] nums) {
+        int len = nums.length;
+        int[][] dp = new int[len][len];
+
+        // dp[i][j]：作为先手，在区间 nums[i..j] 里进行选择可以获得的相对分数
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int j = 1; j < len; j++) {
+            for (int i = j - 1; i >= 0; i--) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][len - 1] >= 0;
+    }
+}

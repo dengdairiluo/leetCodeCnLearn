32fef91d8e0931999e44d886f43f2b800b6c7f98
Alex <1020447548@qq.com>
2022-02-07 19:33:03 +0800

    20220207

--- src/main/java/com/yuren/leetcodecnlearn/Q503.java
+++ src/main/java/com/yuren/leetcodecnlearn/Q503.java
@@ -1,66 +1,22 @@
 package com.yuren.leetcodecnlearn;
 
-/**
- * Created with Intellij IDEA.
- * Description:
- *
- * @author lujiang
- * @date 2021-07-23 22:32
- */
+import java.util.Arrays;
+
 public class Q503 {
-    /**
-     * 记录最大深度
-     */
-    private int maxHeight = Integer.MIN_VALUE;
-    /**
-     * 记录bottom left tree value
-     */
-    private int res;
-
-    public int findBottomLeftValue(TreeNode root) {
-        bfs(root, 0);
-        return res;
-    }
-
-    // 要遍历整棵树，所以没有返回值
-    private void bfs(TreeNode root, int height) {
-        // 叶子节点判断
-        if (root.left == null && root.right == null) {
-            if (height > maxHeight) {
-                maxHeight = height;
-                res = root.val;
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        // 使用数组模拟栈，hh 代表栈底，tt 代表栈顶
+        int[] d = new int[n * 2];
+        int hh = 0, tt = -1;
+        for (int i = 0; i < n * 2; i++) {
+            while (hh <= tt && nums[i % n] > nums[d[tt]]) {
+                int u = d[tt--];
+                ans[u] = nums[i % n];
             }
-            // 这里要return
-            return;
+            d[++tt] = i % n;
         }
-
-        // 因为递归的出口在叶子节点，所以不能继续到空节点
-        if (root.left != null) {
-            bfs(root.left, height + 1);
-        }
-
-        if (root.right != null) {
-            bfs(root.right, height + 1);
-        }
-
-    }
-
-    private static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+        return ans;
     }
 }

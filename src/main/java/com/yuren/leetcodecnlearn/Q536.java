2f76e27f6624bf5a30756785ef630118aa07957b
Alex <1020447548@qq.com>
2022-02-22 21:03:57 +0800

    20220222.1

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q536.java
@@ -0,0 +1,55 @@
+package com.yuren.leetcodecnlearn;
+
+public class Q536 {
+    private static int buildSubTree(TreeNode node, String string, int index) {
+        int i = index;
+        while ((string.charAt(i) >= '0' && string.charAt(i) <= '9') || string.charAt(i) == '-') {
+            i++;
+            if (i >= string.length()) {
+                break;
+            }
+        }
+        node.val = Integer.parseInt(string.substring(index, i));
+        if (i < string.length() && string.charAt(i) == '(') {
+            node.left = new TreeNode(0);
+            i = buildSubTree(node.left, string, i + 1);
+        }
+        if (i < string.length() && string.charAt(i) == '(') {
+            node.right = new TreeNode(0);
+            i = buildSubTree(node.right, string, i + 1);
+        }
+        if (i < string.length() && string.charAt(i) == ')') {
+            return ++i;
+        }
+        return i;
+    }
+
+    public TreeNode str2tree(String s) {
+        if (s == null || s.length() < 1) {
+            return null;
+        }
+        TreeNode node = new TreeNode(0);
+        buildSubTree(node, s, 0);
+        return node;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

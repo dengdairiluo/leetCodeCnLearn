bde220ee702138093bc5f1e9556e4c6ffa82c095
dengdairiluo <1020447548@qq.com>
2022-03-03 23:40:04 +0800

    20220303

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q551.java
@@ -0,0 +1,37 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-03-03 23:37
+ */
+public class Q551 {
+    public boolean checkRecord(String s) {
+        int n = s.length();
+        char[] cs = s.toCharArray();
+        for (int i = 0, cnt = 0; i < n; ) {
+            char c = cs[i];
+            if (c == 'A') {
+                cnt++;
+                if (cnt >= 2) {
+                    return false;
+                }
+            } else if (c == 'L') {
+                int j = i;
+                while (j < n && cs[j] == 'L') {
+                    j++;
+                }
+                int len = j - i;
+                if (len >= 3) {
+                    return false;
+                }
+                i = j;
+                continue;
+            }
+            i++;
+        }
+        return true;
+    }
+}

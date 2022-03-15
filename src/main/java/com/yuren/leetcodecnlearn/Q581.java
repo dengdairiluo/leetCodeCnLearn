ea91f2e133df22dd3da517b9d306048fe0d6f4d9
dengdairiluo <1020447548@qq.com>
2022-03-15 23:01:15 +0800

    20220315

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q581.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-03-15 23:00
+ */
+public class Q581 {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
+    }
+}

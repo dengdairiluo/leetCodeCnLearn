ce80b1de1522d4367aba384526b9e49510972d83
dengdairiluo <1020447548@qq.com>
2022-03-16 23:35:22 +0800

    20220316

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q582.java
@@ -0,0 +1,35 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.*;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-03-16 23:31
+ */
+public class Q582 {
+
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < ppid.size(); i++) {
+            if (ppid.get(i) > 0) {
+                List<Integer> l = map.getOrDefault(ppid.get(i), new ArrayList<>());
+                l.add(pid.get(i));
+                map.put(ppid.get(i), l);
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> l = new ArrayList<>();
+        queue.add(kill);
+        while (!queue.isEmpty()) {
+            int r = queue.remove();
+            l.add(r);
+            if (map.containsKey(r)) {
+                queue.addAll(map.get(r));
+            }
+        }
+        return l;
+    }
+}

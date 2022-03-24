4cb9fc6960e4444ac911a002e9e670a461b08dcf
dengdairiluo <1020447548@qq.com>
2022-03-25 05:48:36 +0800

    20220325

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q606.java
@@ -0,0 +1,45 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-03-25 05:43
+ */
+public class Q606 {
+    private final StringBuilder sb = new StringBuilder();
+
+    public String tree2str(TreeNode root) {
+        dfs(root);
+        return sb.substring(1, sb.length() - 1);
+    }
+
+    void dfs(TreeNode root) {
+        sb.append("(");
+        sb.append(root.val);
+        if (root.left != null) dfs(root.left);
+        else if (root.right != null) sb.append("()");
+        if (root.right != null) dfs(root.right);
+        sb.append(")");
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

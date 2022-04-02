e376a06f9debe5c2728a413989a90f9bfb155355
dengdairiluo <1020447548@qq.com>
2022-04-02 23:35:43 +0800

    20220402

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q625.java
@@ -0,0 +1,23 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-04-02 23:34
+ */
+public class Q625 {
+    public int smallestFactorization(int a) {
+        if (a < 2) return a;
+        long res = 0, mul = 1;
+        for (int i = 9; i >= 2; i--) {
+            while (a % i == 0) {
+                a /= i;
+                res = mul * i + res;
+                mul *= 10;
+            }
+        }
+        return a < 2 && res <= Integer.MAX_VALUE ? (int) res : 0;
+    }
+}

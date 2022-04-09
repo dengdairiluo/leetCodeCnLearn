40584538840ee54630365dd573885b1df8072b98
dengdairiluo <1020447548@qq.com>
2022-04-09 23:36:34 +0800

    20220409

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q640.java
@@ -0,0 +1,56 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-04-09 23:34
+ */
+public class Q640 {
+    public static String solveEquation(String equation) {
+        char[] chars = equation.toCharArray();
+        //  x数量  常数和                           单位        遇到等于符号反转
+        int x = 0, result = 0, length = chars.length, symbol = 1, eq = 1;
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            int k = c - '0';
+            switch (c) {
+                case '+':
+                    symbol = eq;
+                    break;
+                case '-':
+                    symbol = -eq;
+                    break;
+                case '=':
+                    eq = -1;
+                    symbol = eq;
+                    break;
+                case 'x':
+                    x += symbol;
+                    break;
+                default:
+                    int z = k, j = i + 1;
+                    for (; j < length; j++) {
+                        if (chars[j] == 'x') {
+                            x += z * symbol;
+                            j = j + 1;
+                            z = 0;
+                            break;
+                        }
+                        k = chars[j] - '0';
+                        if (k >= 0 && k <= 9) {
+                            z = z * 10 + k;
+                        } else {
+                            break;
+                        }
+                    }
+                    i = j - 1;
+                    if (z != 0) result += z * symbol;
+            }
+        }
+        if (x == 0 && result == 0) return "Infinite solutions";
+        if (x == 0) return "No solution";
+        return "x=" + -(result / x);
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q641.java
@@ -0,0 +1,136 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-04-09 23:36
+ */
+public class Q641 {
+    DoublyLinkedList dll;
+
+    public Q641(int k) {
+        dll = new DoublyLinkedList(k);
+    }
+
+    public boolean insertFront(int value) {
+        if (dll.size < dll.capacity) {
+            Node n = new Node(value);
+            dll.insertAtHead(n);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insertLast(int value) {
+        if (dll.size < dll.capacity) {
+            Node n = new Node(value);
+            dll.insertAtTail(n);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteFront() {
+        if (!dll.isEmpty()) {
+            dll.removeAtHead();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteLast() {
+        if (!dll.isEmpty()) {
+            dll.removeAtTail();
+            return true;
+        }
+        return false;
+    }
+
+    public int getFront() {
+        if (dll.isEmpty()) {
+            return -1;
+        }
+        return dll.head.next.val;
+    }
+
+    public int getRear() {
+        if (dll.isEmpty()) {
+            return -1;
+        }
+        return dll.tail.prev.val;
+    }
+
+    public boolean isEmpty() {
+        return dll.isEmpty();
+    }
+
+    public boolean isFull() {
+        return dll.isFull();
+    }
+
+    class Node {
+        int val;
+        Node next, prev;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    class DoublyLinkedList {
+        Node head = new Node(-1);
+        Node tail = new Node(-1);
+        int size;
+        int capacity;
+
+        public DoublyLinkedList(int capacity) {
+            this.head.next = tail;
+            this.tail.prev = head;
+            this.size = 0;
+            this.capacity = capacity;
+        }
+
+        public void insertAtTail(Node n) {
+            Node prev = tail.prev;
+            tail.prev = n;
+            n.next = tail;
+            prev.next = n;
+            n.prev = prev;
+            this.size++;
+        }
+
+        public void insertAtHead(Node n) {
+            Node next = head.next;
+            n.next = next;
+            next.prev = n;
+            n.prev = head;
+            head.next = n;
+            this.size++;
+        }
+
+        public void remove(Node n) {
+            n.next.prev = n.prev;
+            n.prev.next = n.next;
+            this.size--;
+        }
+
+        public void removeAtTail() {
+            remove(tail.prev);
+        }
+
+        public void removeAtHead() {
+            remove(head.next);
+        }
+
+        public boolean isEmpty() {
+            return this.size == 0;
+        }
+
+        public boolean isFull() {
+            return this.size == this.capacity;
+        }
+
+    }
+}

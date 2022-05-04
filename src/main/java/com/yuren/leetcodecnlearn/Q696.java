fa7627a5a732dc33166c5a55aca41f5182cc65c9
dengdairiluo <1020447548@qq.com>
2022-05-04 22:56:34 +0800

    20220504

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q696.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-05-04 22:56
+ */
+public class Q696 {
+    public int countBinarySubstrings(String s) {
+        int ptr = 0, n = s.length(), last = 0, ans = 0;
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            ans += Math.min(count, last);
+            last = count;
+        }
+        return ans;
+    }
+}

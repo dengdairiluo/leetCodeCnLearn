b26e7183eb930a2dd4f0620a060c9614a8b62a54
dengdairiluo <1020447548@qq.com>
2022-05-11 23:46:41 +0800

    20220511

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q714.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-05-11 23:46
+ */
+public class Q714 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+}

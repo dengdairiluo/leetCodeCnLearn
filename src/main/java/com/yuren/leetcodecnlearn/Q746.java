32430dcdad6d1cbe367d5a0b567f0717e5a206e8
dengdairiluo <1020447548@qq.com>
2022-05-26 23:44:01 +0800

    20220526

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q746.java
@@ -0,0 +1,21 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-05-26 23:42
+ */
+public class Q746 {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int prev = 0, curr = 0;
+        for (int i = 2; i <= n; i++) {
+            int next = Math.min(curr + cost[i - 1], prev + cost[i - 2]);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+}

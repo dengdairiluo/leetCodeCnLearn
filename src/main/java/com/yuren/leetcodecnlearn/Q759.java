940c5b14a7f1f440aa19657e74dfd18b20397b5b
dengdairiluo <1020447548@qq.com>
2022-06-01 23:30:59 +0800

    20220601

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q759.java
@@ -0,0 +1,53 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-06-01 23:29
+ */
+public class Q759 {
+    public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
+        int OPEN = 0, CLOSE = 1;
+
+        List<int[]> events = new ArrayList<>();
+        for (List<Interval> employee : avails)
+            for (Interval iv : employee) {
+                events.add(new int[]{iv.start, OPEN});
+                events.add(new int[]{iv.end, CLOSE});
+            }
+
+        events.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        List<Interval> ans = new ArrayList<>();
+
+        int prev = -1, bal = 0;
+        for (int[] event : events) {
+            // event[0] = time, event[1] = command
+            if (bal == 0 && prev >= 0)
+                ans.add(new Interval(prev, event[0]));
+            bal += event[1] == OPEN ? 1 : -1;
+            prev = event[0];
+        }
+
+        return ans;
+    }
+
+    private static class Interval {
+        public int start;
+        public int end;
+
+        public Interval() {
+        }
+
+        public Interval(int _start, int _end) {
+            start = _start;
+            end = _end;
+        }
+    }
+
+    ;
+}

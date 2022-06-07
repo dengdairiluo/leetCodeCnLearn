0cbf016ffcc1cbc86380b862c0205133d6297da6
dengdairiluo <1020447548@qq.com>
2022-06-07 22:22:28 +0800

    20220607

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q775.java
@@ -0,0 +1,22 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-06-07 22:16
+ */
+public class Q775 {
+    public boolean isIdealPermutation(int[] a) {
+        int n = a.length;
+        int floor = n;
+        for (int i = n - 1; i >= 2; --i) {
+            floor = Math.min(floor, a[i]);
+            if (a[i - 2] > floor) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

f2792420bec515482fb958e38b3ef8c6e01394ac
dengdairiluo <1020447548@qq.com>
2022-06-18 22:39:22 +0800

    20220618

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q799.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-06-18 22:38
+ */
+public class Q799 {
+    public double champagneTower(int poured, int queryRow, int queryGlass) {
+        // 创建一个二维数组
+        double[][] arr = new double[queryRow + 2][queryRow + 2];
+        arr[0][0] = poured;
+        // 行遍历
+        for (int i = 0; i <= queryRow; i++) {
+            // 列遍历
+            for (int j = 0; j <= i; j++) {
+                if (arr[i][j] > 1) {
+                    double tmp = arr[i][j] - 1;
+                    arr[i][j] = 1.0;
+                    arr[i + 1][j] += tmp / 2.0;
+                    arr[i + 1][j + 1] += tmp / 2.0;
+                }
+            }
+        }
+        return arr[queryRow][queryGlass];
+    }
+}

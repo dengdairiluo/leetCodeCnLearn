bf31f288c3afd0d17878430afdedc501a39a6e66
dengdairiluo <1020447548@qq.com>
2022-06-26 23:53:24 +0800

    20220626

--- src/main/java/com/yuren/leetcodecnlearn/Q820.java
+++ src/main/java/com/yuren/leetcodecnlearn/Q820.java
@@ -1,5 +1,7 @@
 package com.yuren.leetcodecnlearn;
 
+import java.util.Arrays;
+
 /**
  * Created with Intellij IDEA.
  * Description:
@@ -18,5 +20,47 @@ public class Q820 {
         }
         return xor == 0;
 
+    }
+
+    public int minimumLengthEncoding(String[] words) {
+        Arrays.sort(words, (s1, s2) -> s2.length() - s1.length());
+        int ans = 0;
+        Trie trie = new Trie();
+        for (String word : words) {
+            ans += trie.insert(word);
+        }
+        return ans;
+    }
+
+    private static class Trie {
+        Trie[] children;
+        boolean isWord;
+
+        public Trie() {
+            this.children = new Trie[26];
+            this.isWord = false;
+        }
+
+        /**
+         * 倒着插入新的单词，返回本次插入对有效编码长度的贡献
+         *
+         * @param word
+         * @return
+         */
+        public int insert(String word) {
+            Trie node = this;
+            boolean isNew = false;
+            for (int i = word.length() - 1; i >= 0; i--) {
+                int idx = word.charAt(i) - 'a';
+                if (node.children[idx] == null) {
+                    // 是新单词
+                    isNew = true;
+                    node.children[idx] = new Trie();
+                }
+                node = node.children[idx];
+            }
+            node.isWord = true;
+            return isNew ? word.length() + 1 : 0;
+        }
     }
 }

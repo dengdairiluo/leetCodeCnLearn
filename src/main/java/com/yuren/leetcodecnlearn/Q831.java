50fe4a9d1f9c8fd58bd5b9577f0581f7c43c8393
dengdairiluo <1020447548@qq.com>
2022-07-01 23:42:51 +0800

    20220630,20220701

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q831.java
@@ -0,0 +1,40 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-01 23:42
+ */
+public class Q831 {
+    public String maskPII(String s) {
+        int in = s.indexOf('@');
+        if (in > 0) {
+            s = s.toLowerCase();
+            StringBuilder sb = new StringBuilder(s);
+            sb.replace(1, in - 1, "*****");
+            s = sb.toString();
+        } else {
+            s = s.replace("(", "");
+            s = s.replace(")", "");
+            s = s.replace("+", "");
+            s = s.replace("-", "");
+            s = s.replace(" ", "");
+            int l = s.length();
+            if (l == 10) {
+                s = "***-***-" + s.substring(6);
+            }
+            if (l == 11) {
+                s = "+*-***-***-" + s.substring(7);
+            }
+            if (l == 12) {
+                s = "+**-***-***-" + s.substring(8);
+            }
+            if (l == 13) {
+                s = "+***-***-***-" + s.substring(9);
+            }
+        }
+        return s;
+    }
+}

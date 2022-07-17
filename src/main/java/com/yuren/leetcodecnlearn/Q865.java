301bed6a22f2c1671a95a38d0855acd55641ffab
dengdairiluo <1020447548@qq.com>
2022-07-17 23:52:56 +0800

    20220717

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q865.java
@@ -0,0 +1,58 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-17 23:49
+ */
+public class Q865 {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    /**
+     * Return the result of the subtree at this node.
+     *
+     * @param node
+     * @return
+     */
+    public Result dfs(TreeNode node) {
+        if (node == null) return new Result(null, 0);
+        Result L = dfs(node.left),
+                R = dfs(node.right);
+        if (L.dist > R.dist) return new Result(L.node, L.dist + 1);
+        if (L.dist < R.dist) return new Result(R.node, R.dist + 1);
+        return new Result(node, L.dist + 1);
+    }
+
+    private static class Result {
+        TreeNode node;
+        int dist;
+
+        Result(TreeNode n, int d) {
+            node = n;
+            dist = d;
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

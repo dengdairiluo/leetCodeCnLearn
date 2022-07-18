4d10a307e3732a3227ac685d467a49aaa77c6a06
dengdairiluo <1020447548@qq.com>
2022-07-18 22:05:19 +0800

    20220718

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q867.java
@@ -0,0 +1,21 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-18 22:03
+ */
+public class Q867 {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q868.java
@@ -0,0 +1,24 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-18 22:05
+ */
+public class Q868 {
+    public int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for (int i = 0; n != 0; ++i) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    ans = Math.max(ans, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+        }
+        return ans;
+    }
+}

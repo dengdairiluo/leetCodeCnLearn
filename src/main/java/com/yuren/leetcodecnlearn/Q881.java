7813691ad31b0a0b0cc72c625ff9a46270b71aac
dengdairiluo <1020447548@qq.com>
2022-07-24 19:38:03 +0800

    20220724

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q881.java
@@ -0,0 +1,26 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-24 19:33
+ */
+public class Q881 {
+    public int numRescueBoats(int[] people, int limit) {
+        int ans = 0;
+        Arrays.sort(people);
+        int light = 0, heavy = people.length - 1;
+        while (light <= heavy) {
+            if (people[light] + people[heavy] <= limit) {
+                ++light;
+            }
+            --heavy;
+            ++ans;
+        }
+        return ans;
+    }
+}

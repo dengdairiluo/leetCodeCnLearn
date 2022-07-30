8119162ad5758d52da0425b2e3f487a948fe3da7
dengdairiluo <1020447548@qq.com>
2022-07-30 22:05:59 +0800

    20220729 20220730

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q896.java
@@ -0,0 +1,33 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-07-30 22:00
+ */
+public class Q896 {
+    public boolean isMonotonic(int[] nums) {
+        int tag = 0;
+        if (nums[0] <= nums[nums.length - 1]) {
+            tag = 1;
+        }
+        if (tag == 1) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] < nums[i + 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}

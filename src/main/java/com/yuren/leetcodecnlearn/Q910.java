ba1005de66c91fff1f026ce86836a0a2ba2a46e1
dengdairiluo <1020447548@qq.com>
2022-08-07 23:17:08 +0800

    20220804,20220805

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q910.java
@@ -0,0 +1,26 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-08-07 22:35
+ */
+public class Q910 {
+    public int smallestRangeII(int[] arr, int k) {
+        int N = arr.length;
+        Arrays.sort(arr);
+        int ans = arr[N - 1] - arr[0];
+
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int a = arr[i], b = arr[i + 1];
+            int high = Math.max(arr[N - 1] - k, a + k);
+            int low = Math.min(arr[0] + k, b - k);
+            ans = Math.min(ans, high - low);
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q912.java
@@ -0,0 +1,59 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-08-07 23:16
+ */
+public class Q912 {
+    public int[] sortArray(int[] nums) {
+        heapSort(nums);
+        return nums;
+    }
+
+    public void heapSort(int[] nums) {
+        int len = nums.length - 1;
+        buildMaxHeap(nums, len);
+        for (int i = len; i >= 1; --i) {
+            swap(nums, i, 0);
+            len -= 1;
+            maxHeapify(nums, 0, len);
+        }
+    }
+
+    public void buildMaxHeap(int[] nums, int len) {
+        for (int i = len / 2; i >= 0; --i) {
+            maxHeapify(nums, i, len);
+        }
+    }
+
+    public void maxHeapify(int[] nums, int i, int len) {
+        while ((i << 1) + 1 <= len) {
+            int lSon = (i << 1) + 1;
+            int rSon = (i << 1) + 2;
+            int large;
+            if (lSon <= len && nums[lSon] > nums[i]) {
+                large = lSon;
+            } else {
+                large = i;
+            }
+            if (rSon <= len && nums[rSon] > nums[large]) {
+                large = rSon;
+            }
+            if (large != i) {
+                swap(nums, i, large);
+                i = large;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

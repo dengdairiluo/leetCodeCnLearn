af784a1d90c5fff39073cd4e6d15fffb29c014d2
dengdairiluo <1020447548@qq.com>
2022-08-09 23:54:27 +0800

    20220808,20220809

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q917.java
@@ -0,0 +1,39 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-08-09 23:44
+ */
+public class Q917 {
+    public String reverseOnlyLetters(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int left = 0, right = n - 1;
+        while (true) {
+            // 判断左边是否扫描到字母
+            while (left < right && !Character.isLetter(s.charAt(left))) {
+                left++;
+            }
+            // 判断右边是否扫描到字母
+            while (right > left && !Character.isLetter(s.charAt(right))) {
+                right--;
+            }
+            if (left >= right) {
+                break;
+            }
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+
+    private void swap(char[] arr, int left, int right) {
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}

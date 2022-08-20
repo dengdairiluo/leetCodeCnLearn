a9033bd4f09a6ea0173b82b84c2f217aa5da89de
dengdairiluo <1020447548@qq.com>
2022-08-20 23:59:27 +0800

    20220819 20220820

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q945.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-08-20 23:52
+ */
+public class Q945 {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int pre = nums[0] - 1, ans = 0;
+        for (int i : nums) {
+            if (i > pre) {
+                pre = i;
+            } else {
+                ans += ++pre - i;
+            }
+        }
+        return ans;
+    }
+}

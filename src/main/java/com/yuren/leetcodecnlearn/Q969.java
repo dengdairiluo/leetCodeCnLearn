2817896c96579335683396342b1f74ea39f1d90a
dengdairiluo <1020447548@qq.com>
2022-08-30 23:50:38 +0800

    20220830

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q969.java
@@ -0,0 +1,41 @@
+package com.yuren.leetcodecnlearn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-08-30 23:48
+ */
+public class Q969 {
+    public List<Integer> pancakeSort(int[] arr) {
+        List<Integer> ret = new ArrayList<>();
+        for (int n = arr.length; n > 1; n--) {
+            int index = 0;
+            for (int i = 1; i < n; i++) {
+                if (arr[i] >= arr[index]) {
+                    index = i;
+                }
+            }
+            if (index == n - 1) {
+                continue;
+            }
+            reverse(arr, index);
+            reverse(arr, n - 1);
+            ret.add(index + 1);
+            ret.add(n);
+        }
+        return ret;
+    }
+
+    public void reverse(int[] arr, int end) {
+        for (int i = 0, j = end; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}

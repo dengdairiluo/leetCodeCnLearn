6f9c71b6407045730fc660e926693da9b7da13e9
dengdairiluo <1020447548@qq.com>
2022-10-21 23:29:18 +0800

    20221021

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1095.java
@@ -0,0 +1,60 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-10-21 23:27
+ */
+public class Q1095 {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int l = 0, r = mountainArr.length() - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        int peak = l;
+        int index = binarySearch(mountainArr, target, 0, peak, true);
+        if (index != -1) {
+            return index;
+        }
+        return binarySearch(mountainArr, target, peak + 1, mountainArr.length() - 1, false);
+    }
+
+    public int binarySearch(MountainArray mountainArr, int target, int l, int r, boolean flag) {
+        if (!flag) {
+            target *= -1;
+        }
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int cur = mountainArr.get(mid) * (flag ? 1 : -1);
+            if (cur == target) {
+                return mid;
+            } else if (cur < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * // This is MountainArray's API interface.
+     * // You should not implement it, or speculate about its implementation
+     * interface MountainArray {
+     * public int get(int index) {}
+     * public int length() {}
+     * }
+     */
+    private interface MountainArray {
+        int get(int index);
+
+        int length();
+    }
+}

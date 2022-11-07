af0b64e056d96ac6f43c4c9ebe0dfd61cd1f46db
dengdairiluo <1020447548@qq.com>
2022-11-07 23:51:24 +0800

    20221107

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1133.java
@@ -0,0 +1,23 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-11-07 23:47
+ */
+public class Q1133 {
+    public int largestUniqueNumber(int[] nums) {
+        int[] r = new int[1001];
+        for (int num : nums) {
+            r[num]++;
+        }
+        for (int i = 1000; i >= 0; i--) {
+            if (r[i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

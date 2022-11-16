097e4b08a9ef4f60bfee678215c738a036e891f3
dengdairiluo <1020447548@qq.com>
2022-11-16 23:51:16 +0800

    20221116

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1154.java
@@ -0,0 +1,27 @@
+package com.yuren.leetcodecnlearn;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2022-11-16 23:50
+ */
+public class Q1154 {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            ++amount[1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+}

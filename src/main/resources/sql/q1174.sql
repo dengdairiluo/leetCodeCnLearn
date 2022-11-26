036e6a3e4e5a5876e802d842e166c25d7d40c7d2
dengdairiluo <1020447548@qq.com>
2022-11-26 23:48:23 +0800

    20221125 20221126

---
+++ src/main/resources/sql/q1174.sql
@@ -0,0 +1,6 @@
+select round(sum(order_date = customer_pref_delivery_date) / count(*) * 100, 2) immediate_percentage
+from delivery
+where (customer_id, order_date) in
+      (select customer_id, min(order_date)
+       from delivery
+       group by customer_id)

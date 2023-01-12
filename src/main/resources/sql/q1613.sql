4d5d97c728e304b54ade87626d4f75b058a11559
dengdairiluo <1020447548@qq.com>
2023-01-13 00:21:31 +0800

    20230112

--- src/main/resources/sql/q1613.sql
+++
@@ -1,9 +0,0 @@
-with recursive a as (
-    select 1 as n
-    union all
-    select n+1 from a where n<100
-)
-select n as ids
-from a
-where  n not in (select customer_id from Customers)
-  and n<=(select max(customer_id) from Customers)

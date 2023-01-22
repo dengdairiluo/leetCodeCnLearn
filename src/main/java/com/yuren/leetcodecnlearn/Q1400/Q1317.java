d1e7c65b9cd8b00b4634c844f349a79883c6556b
dengdairiluo <1020447548@qq.com>
2023-01-22 23:26:31 +0800

    20230122

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1400/Q1317.java
@@ -0,0 +1,35 @@
+package com.yuren.leetcodecnlearn.Q1400;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-01-22 23:25
+ */
+public class Q1317 {
+    public int[] getNoZeroIntegers(int n) {
+        int num1 = 1, num2 = n - 1;
+        boolean flag = false;
+        while (num1 <= num2 && !flag) {
+            if (noZero(num1) && noZero(num2)) {
+                flag = true;
+            } else {
+                num1++;
+                num2--;
+            }
+        }
+        return new int[]{num1, num2};
+    }
+
+    public boolean noZero(int num) {
+        while (num > 0) {
+            int remainder = num % 10;
+            if (remainder == 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1400/Q1318.java
@@ -0,0 +1,38 @@
+package com.yuren.leetcodecnlearn.Q1400;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-01-22 23:25
+ */
+public class Q1318 {
+    public int minFlips(int a, int b, int c) {
+        int val1 = 0, val2 = 0, val3 = 0;
+        //记录翻转的次数
+        int ans = 0;
+        while (a != 0 || b != 0 || c != 0) {
+            //获取a,b,c的最低位va11,val2,val3
+            val1 = a & 1;
+            val2 = b & 1;
+            val3 = c & 1;
+            //讨论val3的情况
+            if (val3 == 0) {
+                if (val1 == 1) {
+                    if (val2 == 1) ans += 2;
+                    else ans++;
+                } else if (val2 == 1) {
+                    if (val1 == 1) ans += 2;
+                    else ans++;
+                }
+            } else {
+                if (val1 == 0 && val2 == 0) ans++;
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return ans;
+    }
+}

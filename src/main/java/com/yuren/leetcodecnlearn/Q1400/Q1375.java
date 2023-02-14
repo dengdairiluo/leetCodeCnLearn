f9fa19815a42b84bc0567b66e43de9470e9ebcd8
dengdairiluo <1020447548@qq.com>
2023-02-14 23:52:41 +0800

    20230214

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1400/Q1375.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn.Q1400;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-02-14 23:50
+ */
+public class Q1375 {
+    public int numTimesAllBlue(int[] flips) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < flips.length; i++) {
+            int number = flips[i];
+            if (number > max) {
+                max = number;
+            }
+            if (max == i + 1) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}

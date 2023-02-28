5409d20fab40d6acbda66129b597c5a3dd08b0f3
dengdairiluo <1020447548@qq.com>
2023-02-28 23:06:00 +0800

    20230228

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1500/Q1417.java
@@ -0,0 +1,41 @@
+package com.yuren.leetcodecnlearn.Q1500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-02-28 23:04
+ */
+public class Q1417 {
+    public String reformat(String s) {
+        int sumDigit = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                sumDigit++;
+            }
+        }
+        int sumAlpha = s.length() - sumDigit;
+        if (Math.abs(sumDigit - sumAlpha) > 1) {
+            return "";
+        }
+        boolean flag = sumDigit > sumAlpha;
+        char[] arr = s.toCharArray();
+        for (int i = 0, j = 1; i < s.length(); i += 2) {
+            if (Character.isDigit(arr[i]) != flag) {
+                while (Character.isDigit(arr[j]) != flag) {
+                    j += 2;
+                }
+                swap(arr, i, j);
+            }
+        }
+        return new String(arr);
+    }
+
+    public void swap(char[] arr, int i, int j) {
+        char c = arr[i];
+        arr[i] = arr[j];
+        arr[j] = c;
+    }
+}

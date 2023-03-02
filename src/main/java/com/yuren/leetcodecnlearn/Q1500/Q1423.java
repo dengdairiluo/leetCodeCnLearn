8beb48c4b25cf8f8fb4d2244b11e8a0afc70a7ed
dengdairiluo <1020447548@qq.com>
2023-03-02 23:52:47 +0800

    20230302

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1500/Q1423.java
@@ -0,0 +1,27 @@
+package com.yuren.leetcodecnlearn.Q1500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-03-02 23:52
+ */
+public class Q1423 {
+    public int maxScore(int[] nums, int k) {
+        int n = nums.length, len = n - k;
+        int sum = 0, cur = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        for (int i = 0; i < len; i++) {
+            cur += nums[i];
+        }
+        int min = cur;
+        for (int i = len; i < n; i++) {
+            cur = cur + nums[i] - nums[i - len];
+            min = Math.min(min, cur);
+        }
+        return sum - min;
+    }
+}

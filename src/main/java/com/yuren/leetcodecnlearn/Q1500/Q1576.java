5a0d96745ddc7acabb4f9305381285f905500bcf
dengdairiluo <1020447548@qq.com>
2023-05-03 23:32:15 +0800

    20230503

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1500/Q1576.java
@@ -0,0 +1,27 @@
+package com.yuren.leetcodecnlearn.Q1500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-05-03 23:27
+ */
+public class Q1576 {
+    public String modifyString(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] == '?') {
+                for (char ch = 'a'; ch <= 'c'; ++ch) {
+                    if ((i > 0 && arr[i - 1] == ch) || (i < n - 1 && arr[i + 1] == ch)) {
+                        continue;
+                    }
+                    arr[i] = ch;
+                    break;
+                }
+            }
+        }
+        return new String(arr);
+    }
+}
--- src/main/java/com/yuren/leetcodecnlearn/Q1500/Q1577.java
+++ src/main/java/com/yuren/leetcodecnlearn/Q1500/Q1577.java
@@ -1,9 +1,6 @@
 package com.yuren.leetcodecnlearn.Q1500;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Q1577 {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
@@ -18,5 +15,64 @@ public class Q1577 {
             }
         }
         return ans;
+    }
+
+    public int numTriplets(int[] nums1, int[] nums2) {
+        Map<Long, Integer> map = getMap(nums1);
+        int count = 0;
+        count += counting(map, nums2);
+
+        map = getMap(nums2);
+        count += counting(map, nums1);
+
+        return count;
+    }
+
+    private Map<Long, Integer> getMap(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+
+        for (int x: nums) {
+            long square = (long) x * x;
+            map.put(square, map.getOrDefault(square,0) + 1);
+        }
+        return map;
+    }
+
+    private int counting(Map<Long, Integer> map, int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        Arrays.sort(nums);
+
+        for (long key: map.keySet()) {
+            int count = map.get(key);
+            int left = 0, right = n - 1;
+
+            while (left < right) {
+                long product = (long) nums[left] * nums[right];
+                if (product < key) {
+                    left++;
+                } else if (product > key) {
+                    right--;
+                } else {
+                    if (nums[left] == nums[right]) {
+                        total += count * (right - left + 1) * (right - left) / 2;
+                        break;
+                    } else {
+                        int countLeft = 1;
+                        int countRight = 1;
+
+                        while (nums[left] == nums[++left]) {
+                            countLeft++;
+                        }
+                        while (nums[right] == nums[--right]) {
+                            countRight++;
+                        }
+
+                        total +=  count * countLeft * countRight;
+                    }
+                }
+            }
+        }
+        return total;
     }
 }

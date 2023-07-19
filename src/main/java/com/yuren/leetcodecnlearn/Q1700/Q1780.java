639dd6c22de1d93a9abcb339f88080d4d8c5a455
dengdairiluo <1020447548@qq.com>
2023-07-19 22:12:15 +0800

    20230718

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q1700/Q1780.java
@@ -0,0 +1,20 @@
+package com.yuren.leetcodecnlearn.Q1700;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-07-19 22:12
+ */
+public class Q1780 {
+    public boolean checkPowersOfThree(int n) {
+        while (n != 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}

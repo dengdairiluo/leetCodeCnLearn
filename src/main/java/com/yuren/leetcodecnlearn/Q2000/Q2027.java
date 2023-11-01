3b5c1cb72405b56ae9907660bf528712a1f70bc5
dengdairiluo <1020447548@qq.com>
2023-11-01 23:51:28 +0800

    20231101

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2027.java
@@ -0,0 +1,21 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-11-01 23:51
+ */
+public class Q2027 {
+    public int minimumMoves(String s) {
+        int covered = -1, res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'X' && i > covered) {
+                res++;
+                covered = i + 2;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/resources/sql/Q2000/q2026.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    problem_id
+FROM
+    problems
+WHERE
+            likes / (likes + dislikes) < 0.6
+ORDER BY problem_id;

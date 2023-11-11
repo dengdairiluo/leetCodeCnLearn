c789d9e99bf8336f9eb01c30a87d381f7a935558
dengdairiluo <1020447548@qq.com>
2023-11-11 23:32:25 +0800

    20231111

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2046.java
@@ -0,0 +1,29 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+import com.yuren.leetcodecnlearn.entity.ListNode;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-11-11 23:31
+ */
+public class Q2046 {
+    public ListNode sortLinkedList(ListNode head) {
+        ListNode prev = head, curr = head.next;
+        while (curr != null) {
+            if (curr.val < 0) {
+                ListNode t = curr.next;
+                prev.next = t;
+                curr.next = head;
+                head = curr;
+                curr = t;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}

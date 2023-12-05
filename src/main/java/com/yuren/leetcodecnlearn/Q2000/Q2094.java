256deca700391e6a7c76623400effc6427c69093
dengdairiluo <1020447548@qq.com>
2023-12-05 23:14:05 +0800

    20231205

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2094.java
@@ -0,0 +1,51 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-12-05 23:11
+ */
+public class Q2094 {
+    public int[] findEvenNumbers(int[] digits) {
+        // 100 - 999
+        int[] arr = new int[10];
+        for (int digit : digits) {
+            arr[digit]++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 100; i < 999; i += 2) {
+            int a = i / 100;
+            int b = i / 10 % 10;
+            int c = i % 10;
+
+            arr[a]--;
+            arr[b]--;
+            arr[c]--;
+
+            if (arr[a] >= 0 && arr[b] >= 0 && arr[c] >= 0) {
+                list.add(i);
+            }
+
+            arr[a]++;
+            arr[b]++;
+            arr[c]++;
+        }
+
+        int[] ans = new int[list.size()];
+
+        int index = 0;
+        for (int num : list) {
+            ans[index++] = num;
+        }
+
+        return ans;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2095.java
@@ -0,0 +1,26 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+import com.yuren.leetcodecnlearn.entity.ListNode;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-12-05 23:13
+ */
+public class Q2095 {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}

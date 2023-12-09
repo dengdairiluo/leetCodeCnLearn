a04d76e20aea3ee0dd90d73f9b6d75e38b63371d
dengdairiluo <1020447548@qq.com>
2023-12-09 23:57:21 +0800

    20231208

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2100.java
@@ -0,0 +1,41 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-12-09 23:55
+ */
+public class Q2100 {
+    public List<Integer> goodDaysToRobBank(int[] security, int time) {
+
+        int n = security.length;
+        List<Integer> result = new ArrayList<>();
+        if (time == 0) {
+            for (int i = 0; i < n; ++i)
+                result.add(i);
+            return result;
+        }
+
+        int left = 0, right = 0;
+        for (int i = 1; i < n - time; ++i) {
+            if (security[i] <= security[i - 1])
+                ++left;
+            else
+                left = 0;
+            if (security[i + time - 1] <= security[i + time])
+                ++right;
+            else
+                right = 0;
+            if (left >= time && right >= time)
+                result.add(i);
+        }
+
+        return result;
+
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2000/Q2101.java
@@ -0,0 +1,53 @@
+package com.yuren.leetcodecnlearn.Q2000;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2023-12-09 23:56
+ */
+public class Q2101 {
+    List<Integer>[] g;
+
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length, ans = 0;
+        boolean[] visited = new boolean[n];
+        g = new List[n];
+        Arrays.setAll(g, e -> new ArrayList<>());
+        for (int i = 0; i < n; ++i) {
+            int[] bombI = bombs[i];
+            for (int j = i + 1; j < n; ++j) {
+                int[] bombJ = bombs[j];
+                long distance = ((long) bombI[0] - bombJ[0]) * (bombI[0] - bombJ[0]) + ((long) bombI[1] - bombJ[1]) * (bombI[1] - bombJ[1]);
+                if (distance <= (long) bombI[2] * bombI[2]) {
+                    g[i].add(j);
+                }
+                if (distance <= (long) bombJ[2] * bombJ[2]) {
+                    g[j].add(i);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                ans = Math.max(ans, dfs(i, visited, new boolean[n]));
+            }
+        }
+        return ans;
+    }
+
+    int dfs(int x, boolean[] visited, boolean[] _visited) {
+        if (_visited[x]) return 0;
+        visited[x] = _visited[x] = true;
+        int ans = 1;
+        for (int y : g[x]) {
+            ans += dfs(y, visited, _visited);
+        }
+        return ans;
+    }
+
+}

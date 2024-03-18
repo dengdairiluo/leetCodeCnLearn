592edf8dc4a0d37bdc2516a71c71fe37c394797f
dengdairiluo <1020447548@qq.com>
2024-03-18 23:00:27 +0800

    20240318

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2300/Q2302.java
@@ -0,0 +1,21 @@
+package com.yuren.leetcodecnlearn.Q2300;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-03-18 22:56
+ */
+public class Q2302 {
+    public long countSubarrays(int[] nums, long k) {
+        long ans = 0L, sum = 0L;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k)
+                sum -= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2300/Q2303.java
@@ -0,0 +1,25 @@
+package com.yuren.leetcodecnlearn.Q2300;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-03-18 22:59
+ */
+public class Q2303 {
+    public double calculateTax(int[][] brackets, int income) {
+        double totalTax = 0;
+        int lower = 0;
+        for (int[] bracket : brackets) {
+            int upper = bracket[0], percent = bracket[1];
+            int tax = (Math.min(income, upper) - lower) * percent;
+            totalTax += tax;
+            if (income <= upper) {
+                break;
+            }
+            lower = upper;
+        }
+        return totalTax / 100.0;
+    }
+}

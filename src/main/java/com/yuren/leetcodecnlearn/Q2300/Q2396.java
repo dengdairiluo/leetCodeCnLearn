f60c3c245de7c4301a113028241a9e8f02d8a4fa
dengdairiluo <1020447548@qq.com>
2024-05-05 22:49:40 +0800

    20240505

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2300/Q2396.java
@@ -0,0 +1,14 @@
+package com.yuren.leetcodecnlearn.Q2300;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-05-05 下午10:45
+ */
+public class Q2396 {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2300/Q2397.java
@@ -0,0 +1,37 @@
+package com.yuren.leetcodecnlearn.Q2300;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-05-05 下午10:48
+ */
+public class Q2397 {
+    public int maximumRows(int[][] matrix, int numSelect) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] mask = new int[m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mask[i] += matrix[i][j] << (n - j - 1);
+            }
+        }
+        int res = 0;
+        int cur = 0;
+        int limit = (1 << n);
+        while (++cur < limit) {
+            if (Integer.bitCount(cur) != numSelect) {
+                continue;
+            }
+            int t = 0;
+            for (int j = 0; j < m; j++) {
+                if ((mask[j] & cur) == mask[j]) {
+                    ++t;
+                }
+            }
+            res = Math.max(res, t);
+        }
+        return res;
+    }
+}

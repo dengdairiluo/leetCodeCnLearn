f91e40580eed4f3d8c6d5b5ad1bf9c5cd5709c6b
dengdairiluo <1020447548@qq.com>
2024-07-07 23:58:19 +0800

    20240706

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2500/Q2520.java
@@ -0,0 +1,22 @@
+package com.yuren.leetcodecnlearn.Q2500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-07-07 下午11:56
+ */
+public class Q2520 {
+    public int countDigits(int num) {
+        int t = num;
+        int res = 0;
+        while (t != 0) {
+            if (num % (t % 10) == 0) {
+                res++;
+            }
+            t /= 10;
+        }
+        return res;
+    }
+}

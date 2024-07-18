adfc60d96fc3ca800da0e3f29da62e7a909eb9a3
dengdairiluo <1020447548@qq.com>
2024-07-18 23:39:36 +0800

    20240718

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2500/Q2544.java
@@ -0,0 +1,20 @@
+package com.yuren.leetcodecnlearn.Q2500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-07-18 下午11:37
+ */
+public class Q2544 {
+    public int alternateDigitSum(int n) {
+        int res = 0, sign = 1;
+        while (n > 0) {
+            res += n % 10 * sign;
+            sign = -sign;
+            n /= 10;
+        }
+        return -sign * res;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2500/Q2545.java
@@ -0,0 +1,17 @@
+package com.yuren.leetcodecnlearn.Q2500;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-07-18 下午11:39
+ */
+public class Q2545 {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, (a, b) -> b[k] - a[k]);
+        return score;
+    }
+}

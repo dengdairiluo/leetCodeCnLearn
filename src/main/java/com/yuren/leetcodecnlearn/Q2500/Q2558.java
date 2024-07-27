27a01fb0bd613ad91f0e67100056e828c7e8d778
dengdairiluo <1020447548@qq.com>
2024-07-27 23:55:23 +0800

    20240725

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2500/Q2558.java
@@ -0,0 +1,57 @@
+package com.yuren.leetcodecnlearn.Q2500;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-07-27 下午11:48
+ */
+public class Q2558 {
+    public long pickGifts(int[] gifts, int k) {
+        heapify(gifts); // 原地堆化（最大堆）
+        while (k-- > 0 && gifts[0] > 1) {
+            gifts[0] = (int) Math.sqrt(gifts[0]); // 直接修改堆顶
+            sink(gifts, 0); // 堆化（只需要把 gifts[0] 下沉）
+        }
+
+        long ans = 0;
+        for (int x : gifts) {
+            ans += x;
+        }
+        return ans;
+    }
+
+    // 原地堆化（最大堆）
+    // 堆化可以保证 h[0] 是堆顶元素，且 h[i] >= max(h[2*i+1], h[2*i+2])
+    private void heapify(int[] h) {
+        // 倒着遍历，从而保证 i 的左右子树一定是堆，那么 sink(h, i) 就可以把左右子树合并成一个堆
+        // 下标 >= h.length / 2 的元素是二叉树的叶子，无需下沉
+        for (int i = h.length / 2 - 1; i >= 0; i--) {
+            sink(h, i);
+        }
+    }
+
+    // 把 h[i] 不断下沉，每次找左右儿子中最大的交换，直到 i 的左右儿子都 <= h[i] 时停止
+    private void sink(int[] h, int i) {
+        int n = h.length;
+        while (2 * i + 1 < n) {
+            int j = 2 * i + 1; // i 的左儿子
+            if (j + 1 < n && h[j + 1] > h[j]) { // i 的右儿子比 i 的左儿子大
+                j++;
+            }
+            if (h[j] <= h[i]) { // 说明 i 的左右儿子都 <= h[i]，停止下沉
+                break;
+            }
+            swap(h, i, j); // 下沉
+            i = j;
+        }
+    }
+
+    // 交换 h[i] 和 h[j]
+    private void swap(int[] h, int i, int j) {
+        int tmp = h[i];
+        h[i] = h[j];
+        h[j] = tmp;
+    }
+}
---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2500/Q2559.java
@@ -0,0 +1,52 @@
+package com.yuren.leetcodecnlearn.Q2500;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2024-07-27 下午11:49
+ */
+public class Q2559 {
+    private List<Integer> nums = new ArrayList<>();
+
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        for (int i = 0; i < words.length; ++i) {
+            char a = words[i].charAt(0), b = words[i].charAt(words[i].length() - 1);
+            if (vowels.contains(a) && vowels.contains(b)) {
+                nums.add(i);
+            }
+        }
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; ++i) {
+            int l = queries[i][0], r = queries[i][1];
+            ans[i] = search(r + 1) - search(l);
+        }
+        return ans;
+    }
+
+    private int search(int x) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (nums.get(mid) >= x) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}

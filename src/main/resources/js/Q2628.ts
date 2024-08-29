60f80a166fa14c70f30ea9bfda9956a8819544af
jianglu <1020447548@qq.com>
2024-08-29 23:27:17 +0800

    20240829

---
+++ src/main/resources/js/Q2628.ts
@@ -0,0 +1,13 @@
+function helper(key: string, value: any): any {
+    if (value && typeof value === "object" && !Array.isArray(value))
+        return Object.fromEntries(Object.entries(value).sort());
+    else
+        return value;
+}
+
+function areDeeplyEqual(o1: any, o2: any): boolean {
+    const stringifiedO1 = JSON.stringify(o1, helper);
+    const stringifiedO2 = JSON.stringify(o2, helper);
+
+    return stringifiedO1 === stringifiedO2;
+}
---
+++ src/main/resources/js/Q2629.ts
@@ -0,0 +1,9 @@
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    return function (x) {
+        return functions.reduceRight((p, c) => c(p), x);
+    };
+}
+

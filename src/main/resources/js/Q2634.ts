5f1879cec34a2203f36d8575c2e1a2477a750aea
jianglu <1020447548@qq.com>
2024-09-01 23:18:58 +0800

    20240901

---
+++ src/main/resources/js/Q2634.ts
@@ -0,0 +1,12 @@
+type Fn = (n: number, i: number) => any
+
+
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    const res=[]
+
+    for(const [i,v]of arr.entries()){
+
+        if(fn(v,i)) res.push(v)
+    }
+    return res
+}

b6522a68a70584b1e64e280f9fbb7883341d4afd
jianglu <1020447548@qq.com>
2024-09-14 22:41:39 +0800

    20240914

---
+++ src/main/java/com/yuren/leetcodecnlearn/Q2600/Q2660.java
@@ -0,0 +1,28 @@
+package com.yuren.leetcodecnlearn.Q2600;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author 10204
+ * @date 2024-09-14 22:40
+ */
+public class Q2660 {
+    public int isWinner(int[] player1, int[] player2) {
+        int s1 = score(player1);
+        int s2 = score(player2);
+        return s1 == s2 ? 0 : s1 > s2 ? 1 : 2;
+    }
+
+    public int score(int[] player) {
+        int res = 0;
+        for (int i = 0; i < player.length; i++) {
+            if ((i > 0 && player[i - 1] == 10) || (i > 1 && player[i - 2] >= 10)) {
+                res += 2 * player[i];
+            } else {
+                res += player[i];
+            }
+        }
+        return res;
+    }
+}
